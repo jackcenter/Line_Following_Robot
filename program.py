2121240c8b9495eb0d2ca5e263765cdca2f0cba4
Jack Center <centerjd88@gmail.com>
2020-10-15 08:37:55 -0600

    updated state space

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ import controlpy
 import control
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as sig
 
 
 def main():
@@ -13,10 +14,11 @@ def main():
 
     A = np.array([
         [v_bar, -r/L, r/L],
-        [0, 0, 0],
-        [0, 0, 0]
+        [0, -0.1, 0],
+        [0, 0, -0.1]
     ])
 
+    print(np.linalg.inv(A))
     B = np.array([
         [0, 0],
         [1, 0],
@@ -28,21 +30,28 @@ def main():
     C = np.eye(3)
     D = np.zeros((3, 2))
 
+    # F, G, H, M, dt = sig.cont2discrete((A, B, C, D), .01)
+    # print(F)
+
     Q = np.array([
-        [60, 0, 0],
-        [0, 0.1, 0],
-        [0, 0, 0.1]
+        [100, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
     ])
     r_lqr = 0.01
     R = r_lqr*np.eye(2)
 
     K, S, E = controlpy.synthesis.controller_lqr(A, B, Q, R)
+    # K, S, E = controlpy.synthesis.controller_lqr(F, G, Q, R)
     print(K)
 
     state_space = control.StateSpace(A - B @ K, B, C, D)
+    # state_space = control.StateSpace(F - G @ K, G, H, M)
 
     Z = C @ np.linalg.inv(-A + B @ K) @ B       # Intermediate term
+    # Z = H @ np.linalg.inv(-F + G @ K) @ G  # Intermediate term
     F = np.linalg.inv(Z.T @ Z) @ Z.T
+
     ref = np.array([
         [0],
         [3],
